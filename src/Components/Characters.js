f67aeee88dff90b69b28db3f6164c7af6f54cd5b
Billy Yip <birii.yip@gmail.com>
2022-01-06 14:54:44 -0800

    got the hover char to work

--- src/Components/Characters.js
+++ src/Components/Characters.js
@@ -2,8 +2,20 @@ import React from "react";
 
 function Characters() {
   return (
-    <div>
-      <div class="bannerChiho"></div>;
+    <div class="paper">
+      <div class="title">Characters</div>
+      <p class="stickyChar">
+        <div class="bannerChiho"></div>
+        <div class="hide">
+          Nakano Chiho, your childhood friend and potential girlfriend / wife of
+          Yoshioka-senpai.
+        </div>
+        <div class="bannerChiho"></div>
+        <div class="hide">
+          Nakano Chiho, your childhood friend and potential girlfriend / wife of
+          Yoshioka-senpai.
+        </div>
+      </p>
     </div>
   );
 }
