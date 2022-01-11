84a151097076b5fd9af9ba963ae26068101d0b0e
Billy Yip <birii.yip@gmail.com>
2022-01-11 13:01:29 -0800

    test

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/sentimental-site">
+    <BrowserRouter basename="">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
