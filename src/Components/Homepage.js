f4e350d81d6fb32732c80b32f3cc657e6beb56fd
Billy Yip <birii.yip@gmail.com>
2022-01-11 14:05:09 -0800

    test change a to link

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../img/logo.png";
 import sakura from "../img/sakura_home.jpeg";
 import home from "../img/home.png";
@@ -8,33 +9,33 @@ function Homepage() {
     <div class="white">
       <div class="NavBar">
         <img class="Icon" src={logo}></img>
-        <a class="Split" href="/sentimental-site">
+        <Link class="Split" href="/sentimental-site">
           Top
-        </a>
-        <a class="Split" href="/sentimental-site/concept">
+        </Link>
+        <Link class="Split" href="/sentimental-site/concept">
           Concept
-        </a>
-        <a class="Split" href="/sentimental-site/story">
+        </Link>
+        <Link class="Split" href="/sentimental-site/story">
           Story
-        </a>
-        <a class="Split" href="/sentimental-site/chars">
+        </Link>
+        <Link class="Split" href="/sentimental-site/chars">
           Characters
-        </a>
-        <a class="Split" href="/sentimental-site/graphic">
+        </Link>
+        <Link class="Split" href="/sentimental-site/graphic">
           Graphic
-        </a>
-        <a class="Split" href="/sentimental-site/system">
+        </Link>
+        <Link class="Split" href="/sentimental-site/system">
           System
-        </a>
-        <a class="Split" href="/sentimental-site/download">
+        </Link>
+        <Link class="Split" href="/sentimental-site/download">
           Download
-        </a>
-        <a class="Split" href="/sentimental-site/special">
+        </Link>
+        <Link class="Split" href="/sentimental-site/special">
           Special
-        </a>
-        <a class="Split" href="/sentimental-site/spec">
+        </Link>
+        <Link class="Split" href="/sentimental-site/spec">
           Spec
-        </a>
+        </Link>
       </div>
       <div class="paper">
         <img class="homeImg" src={home}></img>
@@ -56,61 +57,61 @@ function Homepage() {
           <ul>1/4/2022 Website Development began!</ul>
           <ul>
             12/24/2021 Demo Version 1.02 almost out!&nbsp;
-            <a class="nocss" href="/download/">
+            <Link class="nocss" href="/download/">
               More Info
-            </a>
+            </Link>
           </ul>
           <ul>
             10/30/2021 Demo Version 1.01 out now!&nbsp;
-            <a class="nocss" href="/download/">
+            <Link class="nocss" href="/download/">
               More Info
-            </a>
+            </Link>
           </ul>
         </div>
         <div class="container">
-          <a class="nocss" href="/sentimental-site/story/">
+          <Link class="nocss" href="/sentimental-site/story/">
             <div class="banner">Story</div>
-          </a>
-          <a class="nocss" href="/sentimental-site/chars/">
+          </Link>
+          <Link class="nocss" href="/sentimental-site/chars/">
             <div class="banner">Characters</div>
-          </a>
+          </Link>
         </div>
         <div class="container">
-          <a class="nocss" href="/sentimental-site/download/">
+          <Link class="nocss" href="/sentimental-site/download/">
             <div class="banner">Download</div>
-          </a>
-          <a class="nocss" href="/sentimental-site/spec/">
+          </Link>
+          <Link class="nocss" href="/sentimental-site/spec/">
             <div class="banner">About</div>
-          </a>
+          </Link>
         </div>
         <div class="footer">
-          <a class="Split" href="/sentimental-site">
+          <Link class="Split" href="/sentimental-site">
             Top
-          </a>
-          <a class="Split" href="/sentimental-site/concept">
+          </Link>
+          <Link class="Split" href="/sentimental-site/concept">
             Concept
-          </a>
-          <a class="Split" href="/sentimental-site/story">
+          </Link>
+          <Link class="Split" href="/sentimental-site/story">
             Story
-          </a>
-          <a class="Split" href="/sentimental-site/chars">
+          </Link>
+          <Link class="Split" href="/sentimental-site/chars">
             Characters
-          </a>
-          <a class="Split" href="/sentimental-site/graphic">
+          </Link>
+          <Link class="Split" href="/sentimental-site/graphic">
             Graphic
-          </a>
-          <a class="Split" href="/sentimental-site/system">
+          </Link>
+          <Link class="Split" href="/sentimental-site/system">
             System
-          </a>
-          <a class="Split" href="/sentimental-site/download">
+          </Link>
+          <Link class="Split" href="/sentimental-site/download">
             Download
-          </a>
-          <a class="Split" href="/sentimental-site/special">
+          </Link>
+          <Link class="Split" href="/sentimental-site/special">
             Special
-          </a>
-          <a class="Split" href="/sentimental-site/spec">
+          </Link>
+          <Link class="Split" href="/sentimental-site/spec">
             Spec
-          </a>
+          </Link>
         </div>
       </div>
     </div>
